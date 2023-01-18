5b0142c3ad04a5b30f5e6d24a70a9e760a62bdc3
Pedro270707 <pedro2007.prrr@gmail.com>
2023-01-18 02:57:11 -0300

    Update to 1.0.1

--- src/main/java/net/pedroricardo/headed/client/render/entity/model/AllayHeadEntityModel.java
+++ src/main/java/net/pedroricardo/headed/client/render/entity/model/AllayHeadEntityModel.java
@@ -4,22 +4,22 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.entity.model.AllayEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.pedroricardo.headed.client.render.block.entity.HeadedSkullBlockEntityModel;
 
 @Environment(EnvType.CLIENT)
 public class AllayHeadEntityModel extends HeadedSkullBlockEntityModel {
+    private final ModelPart root;
     private final ModelPart head;
 
     public AllayHeadEntityModel(ModelPart root) {
-        this.head = root.getChild("head");
+        this.root = root.getChild("root");
+        this.head = this.root.getChild("head");
     }
 
     public static TexturedModelData getTexturedModelData() {
-        ModelData modelData = new ModelData();
-        ModelPartData modelPartData = modelData.getRoot();
-        modelPartData.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(-2.5F, -5.0F, -2.5F, 5.0F, 5.0F, 5.0F), ModelTransform.NONE);
-        return TexturedModelData.of(modelData, 32, 32);
+        return AllayEntityModel.getTexturedModelData();
     }
 
     public void setHeadRotation(float animationProgress, float yaw, float pitch) {
@@ -28,6 +28,7 @@ public class AllayHeadEntityModel extends HeadedSkullBlockEntityModel {
     }
 
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
+        matrices.translate(0.0F, 0.25F, 0.0F);
         this.head.render(matrices, vertices, light, overlay, red, green, blue, alpha);
     }
 }
