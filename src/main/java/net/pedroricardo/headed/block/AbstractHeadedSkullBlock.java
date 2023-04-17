f9af86c2788697a997c149301ec95c53a6b7ac25
Pedro270707 <pedro2007.prrr@gmail.com>
2023-04-17 19:08:25 -0300

    Sheep heads can now be redyed

--- src/main/java/net/pedroricardo/headed/block/AbstractHeadedSkullBlock.java
+++ src/main/java/net/pedroricardo/headed/block/AbstractHeadedSkullBlock.java
@@ -11,13 +11,20 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.Wearable;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import net.pedroricardo.headed.HeadedConfig;
 import net.pedroricardo.headed.block.entity.HeadedBlockEntities;
 import net.pedroricardo.headed.block.entity.HeadedSkullBlockEntity;
@@ -64,6 +71,165 @@ public abstract class AbstractHeadedSkullBlock extends BlockWithEntity implement
                 ((HeadedSkullBlockEntity) blockEntity).setCustomName(itemStack.getName());
             }
         }
+    }
+
+    private ActionResult dyeActionResult(PlayerEntity player, Hand hand, World world, BlockPos pos) {
+        if (!player.isCreative())
+            player.getStackInHand(hand).decrement(1);
+        world.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_DYE_USE, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+        player.incrementStat(Stats.USED.getOrCreateStat(player.getStackInHand(hand).getItem()));
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
+                              BlockHitResult hit) {
+        if (state.isOf(HeadedBlocks.WHITE_SHEEP_HEAD) || state.isOf(HeadedBlocks.ORANGE_SHEEP_HEAD) || state.isOf(HeadedBlocks.MAGENTA_SHEEP_HEAD) || state.isOf(HeadedBlocks.LIGHT_BLUE_SHEEP_HEAD) || state.isOf(HeadedBlocks.YELLOW_SHEEP_HEAD) || state.isOf(HeadedBlocks.LIME_SHEEP_HEAD) || state.isOf(HeadedBlocks.PINK_SHEEP_HEAD) || state.isOf(HeadedBlocks.GRAY_SHEEP_HEAD) || state.isOf(HeadedBlocks.LIGHT_GRAY_SHEEP_HEAD) || state.isOf(HeadedBlocks.CYAN_SHEEP_HEAD) || state.isOf(HeadedBlocks.PURPLE_SHEEP_HEAD) || state.isOf(HeadedBlocks.BLUE_SHEEP_HEAD) || state.isOf(HeadedBlocks.BROWN_SHEEP_HEAD) || state.isOf(HeadedBlocks.GREEN_SHEEP_HEAD) || state.isOf(HeadedBlocks.RED_SHEEP_HEAD) || state.isOf(HeadedBlocks.BLACK_SHEEP_HEAD)) {
+            if (world.isClient)
+                return ActionResult.CONSUME;
+            else {
+                int rotation = state.get(Properties.ROTATION);
+                if (player.getStackInHand(hand).isOf(Items.WHITE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.WHITE_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                else if (player.getStackInHand(hand).isOf(Items.ORANGE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.ORANGE_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.MAGENTA_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.MAGENTA_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.LIGHT_BLUE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.LIGHT_BLUE_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.YELLOW_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.YELLOW_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.LIME_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.LIME_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.PINK_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.PINK_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.GRAY_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.GRAY_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.LIGHT_GRAY_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.LIGHT_GRAY_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.CYAN_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.CYAN_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.PURPLE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.PURPLE_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.BLUE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.BLUE_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.BROWN_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.BROWN_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.GREEN_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.GREEN_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.RED_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.RED_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.BLACK_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.BLACK_SHEEP_HEAD.getDefaultState().with(Properties.ROTATION, rotation));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+
+                return ActionResult.FAIL;
+            }
+        } else if (state.isOf(HeadedBlocks.WHITE_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.ORANGE_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.MAGENTA_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.LIGHT_BLUE_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.YELLOW_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.LIME_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.PINK_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.GRAY_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.LIGHT_GRAY_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.CYAN_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.PURPLE_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.BLUE_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.BROWN_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.GREEN_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.RED_SHEEP_WALL_HEAD) || state.isOf(HeadedBlocks.BLACK_SHEEP_WALL_HEAD)) {
+            if (world.isClient)
+                return ActionResult.SUCCESS;
+            else {
+                Direction facing = state.get(Properties.FACING);
+                if (player.getStackInHand(hand).isOf(Items.WHITE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.WHITE_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.ORANGE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.ORANGE_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.MAGENTA_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.MAGENTA_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.LIGHT_BLUE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.LIGHT_BLUE_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.YELLOW_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.YELLOW_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.LIME_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.LIME_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.PINK_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.PINK_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.GRAY_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.GRAY_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.LIGHT_GRAY_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.LIGHT_GRAY_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.CYAN_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.CYAN_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.PURPLE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.PURPLE_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.BLUE_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.BLUE_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.BROWN_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.BROWN_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.GREEN_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.GREEN_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.RED_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.RED_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                if (player.getStackInHand(hand).isOf(Items.BLACK_DYE)) {
+                    world.setBlockState(pos, HeadedBlocks.BLACK_SHEEP_WALL_HEAD.getDefaultState().with(Properties.FACING, facing));
+                    return dyeActionResult(player, hand, world, pos);
+                }
+                return ActionResult.FAIL;
+            }
+        }
+        return ActionResult.FAIL;
     }
 
     public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {
